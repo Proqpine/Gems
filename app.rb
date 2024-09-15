bb2225a1e853cbcb3c45e30e047ec0923b6a32dd
Ayomided <adediji.d@yahoo.com>
2024-09-15 14:39:12 +0100

    transcribing podcasts with openai

--- app.rb
+++ app.rb
@@ -1,35 +1,85 @@
 # frozen_string_literal: true
 
-if ARGV.length != 2
-  puts 'Need only one file'
-  exit
+require 'rss'
+require 'open-uri'
+require 'openai'
+require 'net/http'
+require 'fileutils'
+
+url = 'https://feeds.simplecast.com/XA_851k3'
+
+OpenAI.configure do |config|
+  config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+  config.log_errors = true
 end
 
-filename = ARGV[1]
-fh = File.open(filename)
-
-def read_bytes(data)
-  data.readbyte
+def sanitize_filename(file_name)
+  file_name.gsub(/\s+/, '_')
 end
 
-def read_line(data)
-  data.readline('\n')
+def download(url, file_name, directory)
+  sanitized_file_name = sanitize_filename(file_name)
+  path = File.join(directory, "#{sanitized_file_name}.mp3")
+  FileUtils.mkdir_p(directory) unless File.directory?(directory)
+
+  case io = URI.open(url)
+  when StringIO
+    File.open(path, 'wb') { |f| f.write(io.read) }
+  when Tempfile
+    io.close
+    FileUtils.mv(io.path, path)
+  end
+  path
 end
 
-option = ARGV[0]
-out = option.split('')
+def split_audio(file_path, chunk_duration = 300)
+  output_pattern = "#{File.basename(file_path, '.*')}_%03d#{File.extname(file_path)}"
+  command = "ffmpeg -i #{file_path} -f segment -segment_time #{chunk_duration} -c copy #{output_pattern}"
 
-out.each do |i|
-  case i
-  when 'c'
-    puts read_bytes(fh)
-  when 'l'
-    puts read_line(fh).length
-  when 'w'
-    puts 'I count words'
+  unless system(command)
+    puts "FFmpeg command failed: #{command}"
   end
 end
 
-puts filename
+def transcribe(file_path, title)
+  client = OpenAI::Client.new
+  split_audio(file_path, 300)
+  FileUtils.mkdir_p('transcripts') unless File.directory?('transcripts')
 
-fh.close
+  Dir.glob("#{File.basename(file_path, '.*')}_*#{File.extname(file_path)}").each do |chunk_file|
+    File.open(chunk_file, 'rb') do |file|
+      begin
+        response = client.audio.transcribe(
+          parameters: {
+            model: 'whisper-1',
+            file: file,
+            language: 'en'
+          }
+        )
+        File.write("transcripts/#{title}", response['text'], mode: 'a')
+        puts "Transcription for #{chunk_file}: #{response['text']}"
+      rescue => e
+        puts "Error transcribing #{chunk_file}: #{e.message}"
+      end
+    end
+  end
+end
+
+def get_script(item)
+  @limit_episode ||= 0
+  if @limit_episode < 2
+    file_path = download(item.enclosure.url, item.title, 'episodes')
+    transcribe(file_path, item.title)
+    @limit_episode += 1
+  else
+    exit
+  end
+end
+
+URI.open(url) do |rss|
+  feed = RSS::Parser.parse(rss)
+  puts "Title: #{feed.channel.title}"
+  feed.items.each do |item|
+    get_script(item)
+  end
+end
